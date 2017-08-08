ca97f1bfb009eb6fbf9c6fed80e5b6efd49a15c5
Hugo Yu <guo.yu@uwaterloo.ca>
2017-08-08 16:47:09 -0400

    v0.1 - App bar added.

--- app/src/main/java/info/hugoyu/dashclockweather/MyPreferenceActivity.java
+++ app/src/main/java/info/hugoyu/dashclockweather/MyPreferenceActivity.java
@@ -1,13 +1,29 @@
 package info.hugoyu.dashclockweather;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-public class MyPreferenceActivity extends PreferenceActivity {
+public class MyPreferenceActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+        setContentView(R.layout.activity_settings);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        mToolbar.setTitle(getString(R.string.pref_bar_title));
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        getFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new MyPreferenceFragment()).commit();
     }
 }
